1cd4b99909a4c6ba12107248885472448d8545bd
Alex Arena <im@alexarena.com>
2017-02-28 16:00:11 -0700

    added basic support for multiple message types per file

--- correspondent.js
+++ correspondent.js
@@ -1,89 +1,96 @@
-//var messages = require('./ugh.json')
-var unknown_msg = "Unknown error."
 
+//Handles C# style formatted strings
 function parse(str,vars) {
-    var args = [].slice.call(vars, 1)
-    for(var i=0; i<args.length; i++){
-      var regex = new RegExp("{["+i+"]\}","g")
+    let args = [].slice.call(vars, 1)
+    for(let i=0; i<args.length; i++){
+      let regex = new RegExp("{["+i+"]\}","g")
       str = str.replace(regex, args[i])
     }
     return str
 }
 
-var config = function(sourceJSON){
-
-//  console.log('INPUT: ' + input)
-
-  return function(code){
-  //  console.log('hello' + input)
-    return msg(sourceJSON,code,arguments)
+function validate(sourceJSON){
+  let json = null
+  try {
+      json = require(sourceJSON)
+  } catch (e) {
+    throw new Error(e)
+    return 0
   }
 
-  // if(message_file){
-  //   messages = require(message_file)
-  // }
-  // else{
-  //   messages = require('./messages.json')
-  // }
-
-  var firstMsgArr = messages[Object.keys(messages)[0]].msg
-  if(!(  typeof(messages) === 'object' && Array.isArray(firstMsgArr) )){
-    const err = new Error('Missing required JSON fields for uniform-messenger')
-    throw err
+  if(json.messages){
+    if(!Array.isArray(json.messages)){
+      throw new Error('Missing "messages" array required by node-correspondent')
+      return 0
+    }else{
+      return 1
+    }
+  }
+  else{
+    Object.keys(json).forEach((key) => {
+      if(key != 'unknown'){
+        if(!Array.isArray(json[key].messages)){
+          const e = 'Missing "messages" array required by node-correspondent for message type: "'+key+'" '
+          throw new Error(e)
+          return 0
+        }
+      }
+    })
+    return 2
   }
 
-
-  // if(){
-  //   console.log('seems valid')
-  // }
-
-  if(messages['unknown']){
-    unknown_msg = messages['unknown']
-  }
-
-  // if(typeof(tmp) === array){
-  //   console.log('seems valid')
-  // }
-
-
+  return 1
 }
 
-var msg = function(sourceJSON,code,args){
-  var messages = require(sourceJSON)
-  // console.log('SOURCE IS: ' + sourceJSON)
-  // console.log('CODE IS: ' + code)
-  // console.log('ARGS ARE: ')
-  // console.log(args)
-  //console.log()
-  config()
+var msg = (sourceJSON,code,args,mtype) =>{
+  let json = require(sourceJSON)
+
+  if(mtype){
+    json = json[mtype]
+  }
+
   try{
     var msg = ""
-    if(messages.prefix){
-      msg += messages.prefix
+    if(json.prefix){
+      msg += json.prefix
     }
 
-    for(var i=0; i<messages.msg.length; i++){
-      if(messages.msg[i][code]){
-        msg += messages.msg[i][code] //[code
+    for(let i=0; i<json.messages.length; i++){
+      if(json.messages[i][code]){
+        msg += json.messages[i][code] //[code
       }
     }
 
-    if(messages.postfix){
-      msg += messages.postfix
+    if(json.postfix){
+      msg += json.postfix
     }
 
     if(arguments.length >= 2){
        msg = parse(msg, args)
     }
-//    console.log('MSG: ' + msg)
     return msg
   }
   catch(e){
-    console.log('Err e: ' + e)
-    return unknown_msg
+    console.log(e)
+    console.log('Your JSON might not be correctly formatted for node-correspondent.')
+    return ""
   }
 }
 
+module.exports = (sourceJSON) => {
+    let type = validate(sourceJSON)
+
+    if(type === 1){
+      return function(code){
+        return msg(sourceJSON,code,arguments)
+      }
+    }
+    else{
+      return function(mtype,code){
+        return msg(sourceJSON,code,arguments,mtype)
+      }
+    }
+
+}
+
 module.exports.msg = msg
-module.exports = config
-//module.exports.msg = msg
--- demo.js
+++ demo.js
@@ -1,5 +1,7 @@
-var err = require('./correspondent')('./err')
+//var err = require('./correspondent')('./err')
 var success = require('./correspondent')('./success')
+
+var msg = require('./correspondent')('./old')
 
 //.config('./something.json')
 
@@ -7,3 +9,7 @@ var success = require('./correspondent')('./success')
 
 //console.log(err('USER_NOT_FOUND','Alex','Alex2'))
 console.log(success('USER_CREATED','aarena1','hello','world'))
+
+console.log(msg('err','USER_NOT_FOUND','Something','Something else...'))
+
+// setInterval( ()=>{ console.log('yo') }, 3000);
